a7176634a03ecb698c9e36612df886db8e2ff3e9
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-03-22 10:12:16 +0200

    formatting

--- lab3.cpp
+++ lab3.cpp
@@ -3,28 +3,33 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    string s1,s2;
-    string vowels ="aeiouAEIOU";
-    int vCount=0;
+    string s1, s2;
+    string vowels = "aeiouAEIOU";
+    int vCount = 0;
 
     // Reverse String
-    cout<<"Enter a word: ";cin>>s1;
-    for(int i=s1.length()-1;i>=0;i--){
-        s2+=s1[i];
+    cout << "Enter a word: ";
+    cin >> s1;
+    for (int i = s1.length() - 1; i >= 0; i--)
+    {
+        s2 += s1[i];
     }
-    cout<<"The reversed string: "<<s2<<endl;
+    cout << "The reversed string: " << s2 << endl;
 
     // Check Vowels
-    for(int i=0;i<s1.length();i++){
-        if(vowels.find(s1[i])!= string::npos){
+    for (int i = 0; i < s1.length(); i++)
+    {
+        if (vowels.find(s1[i]) != string::npos)
+        {
             vCount++;
         }
     }
-    cout<<"The vowels' count is: "<<vCount<<endl;
+    cout << "The vowels' count is: " << vCount << endl;
     // Plindrome
-    cout<<(s1==s2?"Palindrome":"Not Palindome")<<endl;
+    cout << (s1 == s2 ? "Palindrome" : "Not Palindome") << endl;
 
     return 0;
 }

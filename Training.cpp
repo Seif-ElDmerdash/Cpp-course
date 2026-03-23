6eccbc19ad8cce4545d40d910aebfa8f2e867511
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-03-23 17:52:41 +0200

    columns &Triangle

--- Training.cpp
+++ Training.cpp
@@ -203,7 +203,7 @@
 //     cout << "Enter the symbol: " , cin >> symbol;
 
 //     for(int i=1; i<=rows;i++){
-//         for (int j=1; j<=i; j++){
+//         for (int j=1; j<=columns; j++){
 //             cout << symbol;
 //         }
 //         cout <<"\n";

48a0cf9d6b8cd4ea6807c77b8d8072edda26d466
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-03-23 15:21:05 +0200

    making test cases

--- GetSkilled_Homeworks/Lecture 2/homework3.cpp
+++ GetSkilled_Homeworks/Lecture 2/homework3.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int even1, even2, even3, even4;
 int odd1, odd2, odd3, odd4;
-// 10 19 16 13 14 25 27 28
+
+// Test Case 1: 10 -10 20 -20 0 5 -5 0
+// even_sum = -25, odd_sum = 25
+
+// Test Case 2: -100 -1 -200 -2 -300 -3 -400 -4
+// even_sum = -10, odd_sum = -1000
+
+// Test Case 3: -3 -3 -3 -3 -3 -3 -3 -3
+// even_sum = -12, odd_sum = -12
+
+// Test Case 4: 11 12 13 14 15 16 17 18
+// even_sum = 60, odd_sum = 56
+
+// Test Case 5: 1000 1 1000 1 1000 1 1000 1
+// even_sum = 4, odd_sum = 4000
+
+// Test Case 6: 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000
+// even_sum = overflow, odd_sum = overflow
+
+// Test Case 7: 9 -3 14 0 -8 7 2 -5
+// even_sum = -1, odd_sum = 17
+
+// Test Case 8: 5 0 5 0 5 0 5 0
+// even_sum = 0, odd_sum = 20
 
 int main()
 {

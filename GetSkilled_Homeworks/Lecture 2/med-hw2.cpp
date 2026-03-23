497fab9742fe2bf78b0f8d66d4a9ad30b78eabde
Seif El Dmerdash <S.Mohamed2533@nu.edu.eg>
2026-03-23 15:29:57 +0200

    Applying test cases

--- GetSkilled_Homeworks/Lecture 2/med-hw2.cpp
+++ GetSkilled_Homeworks/Lecture 2/med-hw2.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+// Test Case 1: 1 10
+// normal_result = 55, users_result = 55
+
+// Test Case 2: 10 100
+// normal_result = 5050, users_result = 5005
+
+// Test Case 3: 50 50
+// normal_result = 1275, users_result = 50
+
+// Test Case 4: 0 9
+// normal_result = 45, users_result = 45
+
+// Test Case 5: 10 0 (Outside the constrains)
+// normal_result = 0, users_result = -45
+
+// Test Case 6: 2 1000
+// normal_result = 500500, users_result = 500499
+
+// Test Case 7: -5 -10 (Outside teh constrains)
+// normal_result = 45, users_result = 35
+
 int main()
 {
 

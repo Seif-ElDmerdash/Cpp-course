19430ad7cb87737fd1678730a9f30ec611ee5a04
Your Name <S.Mohamed2533@nu.edu.eg>
2026-04-08 13:23:21 +0200

    Employee Class problem

--- Midterm-Revision.cpp
+++ Midterm-Revision.cpp
@@ -294,58 +294,289 @@
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
+// class Students{
 
-class Students{
+//     private:
 
-    private:
+//         int ID;
+//         string st_name;
+//         double marks;
 
-        int ID;
-        string st_name;
-        double marks;
+//     public:
 
-    public:
+//         void SetInfo(){
+//             cout << "Enter Student ID: ";
+//             cin>>ID;
+//             cout << "Enter Student Name: ";
+//             cin.ignore();
+//             getline(cin, st_name);
+//             cout << "Enter Marks: ";
+//             cin >> marks;
+//         }
 
-        void SetInfo(){
-            cout << "Enter Student ID: ";
-            cin>>ID;
-            cout << "Enter Student Name: ";
-            cin.ignore();
-            getline(cin, st_name);
-            cout << "Enter Marks: ";
-            cin >> marks;
-        }
+//         void displayinfo(){
+//             cout<<"ID: "<<ID<<endl
+//                 <<"Name: "<<st_name<<endl
+//                 <<"Grade: "<<marks<<endl
+//                 <<"GPA: "<<get_marks()<<endl;
+//         }
 
+//         char get_marks(){
+//             if(marks>=85){return 'A';}
+//             else if(marks>=70){return 'B';}
+//             else if(marks>50){return 'C';}
+//             else{return 'F';}
+//         };
+
+// };
+
+// int main(){
+
+//     Students st1;
+//     Students st2;
+
+//     st1.SetInfo();
+//     st2.SetInfo();
+
+//     st1.displayinfo();
+//     st2.displayinfo();
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int N;
+
+//     cout << "Enter size of array: ";
+//     cin >> N;
+
+//     int array[N];
+
+//     for(int i=0; i<N;i++){
+//         cin>>array[i];
+//      
+        // }
+//     for (int i = 0; i < N; i++){
+//      if (i%2 != 0){
+//             cout<<array[i]<<endl;
+//         }
+//         else{
+//             continue;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     int a[n];
+
+//     for (int i = 0; i < n; i++) {
+//         cin >> a[i];
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         if (i % 2 != 0) {
+//             cout << a[i] << " ";
+//         }
+//     }
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int N;
+//     int nums;
+//     int largest{0};
+
+//     cout<<"Enter how many numbers to enter: ";
+//     cin>>N;
+
+//     for(int i = 0 ;i<N;i++){
         
-        void displayinfo(){
-            cout<<"ID: "<<ID<<endl
-                <<"Name: "<<st_name<<endl
-                <<"Grade: "<<marks<<endl
-                <<"GPA: "<<get_marks()<<endl;
-        }
+//         cout<<"Enter the numbers: ";
+//         cin>>nums;
+//         if(nums>largest){
+//             largest = nums;
+//         }
+//     }
+//     cout<<largest;
+// }
 
-        char get_marks(){
-            if(marks>=85){return 'A';}
-            else if(marks>=70){return 'B';}
-            else if(marks>50){return 'C';}
-            else{return 'F';}
-        };
+//Question 2: Sum of Odd Numbers until -1
+//Write a program that keeps taking integers until the user enters -1, then displays the sum of all odd numbers entered.
 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     int N;
+//     int sum=0;
+
+//     while(true){
+//         cout<<"Enter numbers (-1 to stop): ";
+//         cin>>N;
+        
+//         if(N== -1){
+//             break;
+//         }
+//         if(N%2 != 0){
+//             sum+=N;
+//         }
+//     }
+
+//     cout<<sum;
+
+//     return 0;
+// }
+
+
+// Question 3: Factorial CalculationWrite a program that calculates the factorial (n!) of a given positive integer.
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+ 
+//     int N;
+//     double factorial=1;
+
+//     cin>>N;
+
+//     for(int i=1;i<=N;i++){
+//         factorial *= i;
+//     }
+
+//     cout<<factorial;
+
+//     return 0;
+// }
+//Question 4: Average of 5 Grades Write a program that inputs 5 grades and calculates the average. If the average is >= 50, print "Pass", otherwise print "Fail".
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     int grades;
+//     double average;
+//     int sum=0;
+
+//     for(int i =0;i<5;i++){
+
+//         cout<<"Enter a grade: ";
+//         cin>>grades;
+//         sum+=grades;
+//     }
+
+//     average = sum/5;
+
+//     cout<<((average>=50)? "Pass":"Fail");
+
+// }
+
+
+//Question 5: Simple Countdown
+//Write a program that takes a number and counts down to zero using a while loop
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     int number;
+//     cin>>number;
+
+//     while(number>=0){
+
+//         cout<<number--<<endl;
+//     }
+
+//     return 0;
+// }
+
+// Question 6: Circle ClassCreate a Circle class with a private radius. Include methods to set the radius and calculate the area (Area = pi r^2).
+
+// Question 7: Rectangle Class
+// Create a Rectangle class with width and height. Include a method isSquare() that returns true if width equals height.
+
+// Question 8: Car Class
+// Create a Car class with brand and speed. Include a method accelerate() that increases speed by 10.
+
+// Question 9: Account Class
+// Create a BankAccount class with a balance. Include a deposit(double amount) method and a display() method.
+
+//Question 10: Employee Class
+//Create an Employee class with name and salary. If the salary is less than 3000, increase it by 10% using a applyBonus() method.
+
+// #include <iostream>
+// using namespace std;
+
+// class Employee{
+
+//     private:
+
+//         string name;
+//         double salary;
     
-};
+//     public:
 
-int main(){
+//         Employee(){
+//             cout<<"Zyad 3ayiz constructor"<<endl;
+//         }
 
-    Students st1;
-    Students st2;    
+//        void setinfo(string x, double y){
 
-    st1.SetInfo();
-    st2.SetInfo();
+//                 name = x;
+//                 salary = y;
+//        }
 
-    st1.displayinfo();
-    st2.displayinfo();
+//        double applyBonus(){
+//         return (salary*=1.1);
+//        }
 
-    return 0;
-}
+//        string getname(){
+//         return name;
+//        }
+
+//        double getsalary(){
+
+//         if(salary<3000){
+//             applyBonus();
+//         }
+//         return salary;
+//        }
+
+// };
+
+
+// int main(){
+
+//     Employee E1;
+//     Employee E2;
+
+//     E1.setinfo("Kareem",1000.00);
+//     E2.setinfo("Atef",100.50);
+
+//     cout<<E1.getname()<<endl<<E1.getsalary()<<endl;
+//     cout<<E2.getname()<<endl<<E2.getsalary()<<endl;
+
+//     return 0;
+// }

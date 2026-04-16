3682faf81f61b5fff3fbf6f2a5b466e6a6452217
Seif-ElDmerdash <S.mohamed2533@nu.edu.eg>
2026-04-16 22:20:33 +0200

    Drawing X

---
+++ GetSkilled_Homeworks/Lecture 5/For_Loops/hard-hw1.cpp
@@ -0,0 +1,34 @@
+// N = 5
+// Row = N
+// ROW = 1 , STAR at 1 "ROW" STAR at 5 = N+1 - ROW
+// ROW = 2 , STAR at 2 "ROW" STAR at 4 = N+1 - ROW
+// ROW = 3 , STAR at 3 "ROW" STAR at 3 = N+1 - ROW  
+// ROW = 4 , STAR at 4 "ROW" STAR at 2 = N+1 - ROW
+// ROW = 5 , STAR at 5 "ROW" STAR at 1 = N+1 - ROW
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int N;
+    cin >> N;
+
+    for (int row{1}; row <= N; ++row)
+    {
+        for (int column{1}; column <= N; ++column)
+        {
+
+            if (row == column)
+                cout << "*";
+            else if (column == ((N+1) - row))
+                cout << "*";
+            else
+                cout << " ";
+                }
+        cout << endl;
+    }
+
+    return 0;
+}
